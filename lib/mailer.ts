4ca42fece039075cc8d80710f1ef52ac7c2f5d31
bipulsikder <sikder16bipul@gmail.com>
2026-06-12 20:15:03 +0530

    Fix TypeScript error: replace replaceAll with regex replace

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -39,11 +39,11 @@ function getSmtpConfig() {
 
 function escapeHtml(s: string) {
   return s
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#039;")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
 }
 
 const transporter = nodemailer.createTransport(getSmtpConfig())
